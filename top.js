2b028bb4436288583fe31c149eb7bf8e78f88277
cpy917 <phoebegit917@gmail.com>
2025-06-05 02:24:12 +0800

    Add files via upload

---
+++ top.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    $('#BackTop').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 500);
+    });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 3) {
+            $('#BackTop').fadeIn();
+        } else {
+            $('#BackTop').fadeOut();
+        }
+    });
+});
