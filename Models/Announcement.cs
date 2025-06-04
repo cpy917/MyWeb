ada392efa3740c96adba655f2449cfeaacb43cf7
cpy917 <phoebegit917@gmail.com>
2025-06-05 02:11:32 +0800

    Add files via upload

---
+++ Models/Announcement.cs
@@ -0,0 +1,10 @@
+﻿namespace week15.Models
+{
+    public class Announcement
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime PostDate { get; set; }
+    }
+}
